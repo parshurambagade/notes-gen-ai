f76f9516bc1d187a81e28bf547357b7a2a12e699
parshuram-169pi <parshuram@169pi.com>
2025-12-21 22:09:46 +0530

    fix(RecentSavedNotesSection): improve layout and rendering of saved notes
    - Adjust grid layout to center items more effectively.
    - Ensure proper rendering of NotesCard components based on the number of saved notes.
    - Update the Toaster component to use a fixed light theme for consistency.
    
    These changes enhance the user experience by improving the display of recent saved notes and ensuring consistent theming in notifications.

--- components/home/recent-saved-notes-section.tsx
+++ components/home/recent-saved-notes-section.tsx
@@ -30,11 +30,20 @@ const RecentSavedNotesSection = () => {
           Recent Saved Notes
         </h2>
       </div>
-      <div className="max-w-2xl w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4">
+      <div className="max-w-2xl w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center justify-items-center  gap-4">
         {allSavedNotes && allSavedNotes.length > 0 ? (
-          allSavedNotes
-            ?.slice(0, 3)
-            ?.map((notes) => <NotesCard key={notes.id} notes={notes} />)
+          allSavedNotes?.slice(0, 3)?.map((notes) => {
+            return (
+              <div
+                key={notes.id}
+                className={
+                  allSavedNotes.slice(0, 3).length === 1 ? "lg:col-start-2" : ""
+                }
+              >
+                <NotesCard key={notes.id} notes={notes} />
+              </div>
+            );
+          })
         ) : (
           <p className="col-span-full text-center text-gray-500">
             No recent notes available. Create your first note!
